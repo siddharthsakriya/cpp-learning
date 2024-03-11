c9f833e97bf892dddb3f0686e4071e206bc01b65
siddharthsakriya <76904146+siddharthsakriya@users.noreply.github.com>
2024-03-11 17:22:59 +0000

    hello world

---
+++ Lesson 1/hellowworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    //wow this is so advanced and cool 
+
+    /*
+    Wow 
+    this 
+    is 
+    a 
+    multi-line 
+    comment, 
+    wow so cool!!
+    */
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "I am learning C++" << std::endl;
+    std::cout << "I am using a newline character, endline flushes the output buffer apparently" << '\n';
+    return 0;
+}
+
